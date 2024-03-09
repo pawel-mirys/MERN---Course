9370a25d9f1e244ced218e3b550fc35a1eafefe5
Paweł Mirys <pawel.mirys@gmail.com>
2024-03-09 09:26:19 +0100

    update package

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '@/assets/styles/bootstrap.custom.css';
+import '@/assets/styles/index.css';
+import { RouterProvider } from 'react-router-dom';
+import { Router } from '@/router/Router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={Router} />
+  </React.StrictMode>
+);
