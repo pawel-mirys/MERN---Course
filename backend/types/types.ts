53580c5b322ee2d91f0590f433774176b575a4ed
Paweł Mirys <pawel.mirys@gmail.com>
2024-03-09 17:46:46 +0100

    create models

--- backend/types/types.ts
+++ backend/types/types.ts
@@ -1,3 +1,5 @@
+import mongoose from 'mongoose';
+
 export type ProductType = {
   _id: string;
   name: string;
@@ -9,4 +11,55 @@ export type ProductType = {
   countInStock: number;
   rating: number;
   numReviews: number;
+};
+
+export type UserType = {
+  name: string;
+  email: string;
+  password: string;
+  isAdmin: boolean;
+};
+
+export type ReviewType = {
+  name: string;
+  rating: number;
+  comment: string;
+  user: UserType;
+};
+
+export type ProductSchemaType = ProductType & {
+  user: UserType;
+  reviews: ReviewType;
+};
+
+export type OrderType = {
+  user: UserType;
+  orderItems: {
+    name: string;
+    qty: number;
+    image: string;
+    price: number;
+    product: mongoose.Schema.Types.ObjectId;
+  }[];
+  shippingAdress: {
+    adress: string;
+    city: string;
+    postalCode: string;
+    country: string;
+  };
+  paymentMethod: string;
+  paymentResult: {
+    id: string;
+    status: string;
+    update_time: string;
+    email_adress: string;
+  };
+  itemsPrice: number;
+  taxPrice: number;
+  shippingPrice: number;
+  totalPrice: number;
+  isPaid: boolean;
+  paidAt: Date;
+  isDelivered: boolean;
+  deliveredAt: Date;
 };
