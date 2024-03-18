f764ce513002bc6621034ba2b2174d5257827b53
Paweł Mirys <pawel.mirys@gmail.com>
2024-03-18 20:29:09 +0100

    fix user controller response data

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -11,27 +11,25 @@ import {
 } from '../interfaces/userInterfaces.js';
 import generateToken from '../utils/generateToken.js';
 
-const loginUser = asyncHandler(
-  async (
-    req: Request<any, any, LoginRequestBody>,
-    res: Response<{ user: UserType }>
-  ) => {
-    const { email, password } = req.body;
+const loginUser = asyncHandler(async (req: Request, res: Response) => {
+  const { email, password } = req.body;
 
-    const user = await User.findOne({ email: email });
+  const user = await User.findOne({ email: email });
 
-    if (user && (await user.matchPassword(password))) {
-      generateToken(res, user._id);
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
 
-      res.status(200).json({
-        user: user.toObject(),
-      });
-    } else {
-      res.status(401);
-      throw new Error('Invalid email or password');
-    }
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
   }
-);
+});
 
 const registerUser = asyncHandler(async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
